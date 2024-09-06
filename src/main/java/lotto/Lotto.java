7d2feb86c79e4edb270644688ed87d3e3bd9feae
BaSak0630 <babamba125@gmail.com>
2024-09-06 14:04:07 +0900

    LottoService - 금액입력 받기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,15 @@ package lotto;
 
 import java.util.List;
 
+/*
+ * 클래스 이름 Lotto
+ *
+ * 버전 정보 V1
+ *
+ * 날짜 9월 6일
+ *
+ * 저작권 주의
+ */
 public class Lotto {
     private final List<Integer> numbers;
 
---
+++ src/main/java/lotto/LottoService.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+/*
+ * 클래스 이름 LottoService
+ *
+ * 버전 정보 V1
+ *
+ * 날짜 9월 6일
+ *
+ * 저작권 주의
+ */
+public class LottoService {
+    private Integer buyAmount;
+    public void play() {
+        getBuyAmount();
+    }
+
+    private void getBuyAmount() {
+        System.out.println("구입금액을 입력해 주세요");
+        /*try {
+            buyAmount = Integer.parseInt(readLine());
+
+        }catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원으로 나누어 떨어지는 숫자여야합니다.");
+        }*/
+    }
+}
