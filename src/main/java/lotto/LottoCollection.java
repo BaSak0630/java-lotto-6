857169a074eaa8df0d2670b32bca07e907207bf4
BaSak0630 <babamba125@gmail.com>
2024-09-06 14:04:57 +0900

    LottoCollection - Lotto의 집합

---
+++ src/main/java/lotto/LottoCollection.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import java.util.List;
+
+/*
+ * 클래스 이름 LottoCollection
+ *
+ * 버전 정보 V1
+ *
+ * 날짜 9월 6일
+ *
+ * 저작권 주의
+ */
+public class LottoCollection {
+    private List<Lotto> lottos;
+    public LottoCollection(int numberOfLottos) {
+    }
+}
